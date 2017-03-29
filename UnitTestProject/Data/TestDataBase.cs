917bd41f8a10b6546ead4dcdfa53a33572c6edef
Stas Martynov <stas-martynov-98@mail.ru>
2017-03-29 07:34:09 +0300

    Add project for working data base and setup connection.

---
+++ UnitTestProject/Data/TestDataBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VisionBrain.Data;
+
+namespace UnitTestProject.Data
+{
+	[TestClass]
+	public class TestDataBase
+	{
+		[TestInitialize]
+		public void SetUp()
+		{
+
+		}
+		[TestMethod]
+		public void Connection()
+		{
+			bool isConnected = false;
+			try
+			{
+				DataBase.Instance.Connection.Open();
+				DataBase.Instance.Connection.Close();
+				isConnected = true;
+			} catch (Exception ex) {
+				Console.WriteLine(ex.ToString());
+				isConnected = false;
+			}
+			Assert.IsTrue(isConnected);
+		}
+	}
+}
---
+++ VisionBrain/Data/DataBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using FuckingNeuralNetwork.Neural;
+
+namespace VisionBrain.Data
+{
+	public class DataBase
+	{
+		public static DataBase Instance
+		{
+			get
+			{
+				if (_instance == null)
+					_instance = new DataBase();
+				return _instance;
+			}
+			private set { _instance = value; }
+		}
+		private static DataBase _instance;
+		public SqlConnection Connection { get; set; }
+		private String stringConnection = @"Data Source=(localdb)\ProjectsV12;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+		private DataBase()
+		{
+			Connection = new SqlConnection(stringConnection);
+		}
+		public void SaveNeuron()
+		{
+			
+		}
+	}
+}
